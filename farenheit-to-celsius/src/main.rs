e382b4bbd0ed5c82ebf29c0c5ac040dcafd7bdca
= <seanparsons2003@gmail.com>
2025-07-03 16:51:48 +0100

    updated formula to be more accurate, user can now input a value and see the converted value

--- farenheit-to-celsius/src/main.rs
+++ farenheit-to-celsius/src/main.rs
@@ -1,19 +1,17 @@
 use std::io;
-use std::cmp::Ordering;
 
 fn main() {
     loop {
 
         let mut flag = false;
 
+        let mut mode = String::new();
+        
         while flag == false{
 
             println!("Do you want to convert Farenheit to Celsius, or Celsius to Farenheit? 
                 (Type 0 for F to C or type 1 for C to F)");
         
-            let mut mode = String::new();
-                
-
             io::stdin()
                 .read_line(&mut mode)
                 .expect("failed to read line");
@@ -31,6 +29,28 @@ fn main() {
                 Err(_) => {println!("Enter a valid mode!");
                     continue},
             };
+
+            flag = false;
+
+            while flag == false{
+
+                let mut value = String::new();
+                println!("Input the value to convert");
+                    
+                io::stdin()
+                    .read_line(&mut value)
+                    .expect("failed to read line");
+            
+                let value: f32 = match value.trim().parse() {
+                    Ok(num) => {flag = true;
+                        num},
+                    Err(_) => {println!("Enter a number!");
+                        continue},
+                };
+
+                let converted = convert(mode, value);
+                println!("The converted temperature is {converted}");
+            } 
         }
     }
 }
@@ -39,8 +59,8 @@ fn main() {
 //Mode 0 = F to C, Mode 1 = C to F
 fn convert(mode: u8, value: f32) -> f32 {
     if mode == 0 {
-        (value - 30.0)/2.0
+        (value - 32.0)/(9.0/5.0)
     } else {
-        (value*2.0)+30.0
+        (value*(9.0/5.0))+32.0
     }
 } 
