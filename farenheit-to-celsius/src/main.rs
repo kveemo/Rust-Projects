b55f725d5d8252fb698e294848cb108b43123361
= <seanparsons2003@gmail.com>
2025-07-03 16:26:17 +0100

    switched check loop to a while loop

--- farenheit-to-celsius/src/main.rs
+++ farenheit-to-celsius/src/main.rs
@@ -2,20 +2,22 @@ use std::io;
 use std::cmp::Ordering;
 
 fn main() {
-
     loop {
 
-        loop{
+        let mut flag = false;
+
+        while flag == false{
 
             println!("Do you want to convert Farenheit to Celsius, or Celsius to Farenheit? 
                 (Type 0 for F to C or type 1 for C to F)");
-    
+        
             let mut mode = String::new();
-    
+                
+
             io::stdin()
                 .read_line(&mut mode)
                 .expect("failed to read line");
-    
+        
             let mode: u8 = match mode.trim().parse() {
                 Ok(num) => {if num == 0 {
                     println!("Mode 0 selected");
@@ -24,14 +26,15 @@ fn main() {
                     } else { 
                         println!("Enter a valid mode!");
                     }
+                    flag = true;
                     num},
                 Err(_) => {println!("Enter a valid mode!");
                     continue},
             };
-
         }
     }
 }
+
 
 //Mode 0 = F to C, Mode 1 = C to F
 fn convert(mode: u8, value: f32) -> f32 {
