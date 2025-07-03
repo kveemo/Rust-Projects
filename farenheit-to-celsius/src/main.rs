84bd21dd0e54c10a29b08e1c5982240faa7d6018
= <seanparsons2003@gmail.com>
2025-07-03 16:09:35 +0100

    user can select a mode

--- farenheit-to-celsius/src/main.rs
+++ farenheit-to-celsius/src/main.rs
@@ -1,7 +1,36 @@
 use std::io;
+use std::cmp::Ordering;
 
 fn main() {
-    println!("Hello, world!");
+
+    loop {
+
+        loop{
+
+            println!("Do you want to convert Farenheit to Celsius, or Celsius to Farenheit? 
+                (Type 0 for F to C or type 1 for C to F)");
+    
+            let mut mode = String::new();
+    
+            io::stdin()
+                .read_line(&mut mode)
+                .expect("failed to read line");
+    
+            let mode: u8 = match mode.trim().parse() {
+                Ok(num) => {if num == 0 {
+                    println!("Mode 0 selected");
+                    } else if num == 1 {
+                        println!("Mode 1 selected");
+                    } else { 
+                        println!("Enter a valid mode!");
+                    }
+                    num},
+                Err(_) => {println!("Enter a valid mode!");
+                    continue},
+            };
+
+        }
+    }
 }
 
 //Mode 0 = F to C, Mode 1 = C to F
