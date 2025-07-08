26f531cea8e437a7605c6b3d514913cc0b1606c0
= <seanparsons2003@gmail.com>
2025-07-08 09:49:30 +0100

    First commit, array storing song lyrics added

---
+++ twelve_days/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    //just in time for christmas
+    let song_lines: [String; 12] = 
+    ["A partidge in a pear tree.",
+    "Two turtle-doves, and ",
+    "Three French hens, ",
+    "Four calling birds, ",
+    "FIIIIVE GOLDEN RIIIIIIIINGS, ",
+    "Six geese a-laying, ",
+    "Seven swans a-swimming, ",
+    "Eight maids a-milking, ",
+    "Nine ladies dancing, ",
+    "Ten lords a-leaping, ",
+    "Eleven pipers piping, ",
+    "Twelve drummers drumming, "];
+
+
+}
---
+++ twelve_days/target/debug/deps/twelve_days-a6cb510ce7cf2a60.d
@@ -0,0 +1,5 @@
+C:\Users\seanm\Documents\rustProjects\twelve_days\target\debug\deps\twelve_days-a6cb510ce7cf2a60.d: src\main.rs
+
+C:\Users\seanm\Documents\rustProjects\twelve_days\target\debug\deps\libtwelve_days-a6cb510ce7cf2a60.rmeta: src\main.rs
+
+src\main.rs:
